b8fd6d7809c661ef222e53e5a8608d7be909bced
Hady Chakaroun <hadychakaron@gmail.com>
2025-11-24 13:38:48 +0200

    Initial commit - GRPC Calculator project

--- client/client.py
+++ client/client.py
@@ -3,6 +3,7 @@ import calculator_pb2
 import calculator_pb2_grpc
 
 def run():
+    
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = calculator_pb2_grpc.CalculatorStub(channel)
 
